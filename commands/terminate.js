e58e9cb97060513ef024d6158693f408bb5ac34e
banana-king778 <gamingthingemail@gmail.com>
2023-07-23 21:59:26 -0500

    Update terminate.js

--- commands/terminate.js
+++ commands/terminate.js
@@ -6,6 +6,7 @@ module.exports = {
 		.setDescription('Terminate Bot Client'),
 	async execute(interaction) {
         if (interaction.user.id == '624733100064112683') {
+            console.log(`${interaction.user.tag} used /terminate`)
             await interaction.reply('Terminating...')
             process.exit();
         } else {
