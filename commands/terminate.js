e16065f1c1af36ae0548773497c28a45a653a161
Nicholas Liverett <150272237+liverettnicholas@users.noreply.github.com>
2023-11-08 09:48:45 -0600

    Update terminate.js
    added sad face to termination message

--- commands/terminate.js
+++ commands/terminate.js
@@ -7,7 +7,7 @@ module.exports = {
 	async execute(interaction) {
         if (interaction.user.id == '624733100064112683') {
             console.log(`${interaction.user.tag} used /terminate`)
-            await interaction.reply('Terminating...')
+            await interaction.reply(':( Terminating...')
             process.exit();
         } else {
             await interaction.reply('You do not have adequate permission to terminate this bot.')
